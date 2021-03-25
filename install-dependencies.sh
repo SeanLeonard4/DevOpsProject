3f1e32585ddc0480b920a0ed354f5753ce05e492
Ubuntu <ubuntu@ip-172-31-29-98.eu-west-1.compute.internal>
2021-03-25 21:26:16 +0000

    added dependencies

---
+++ install-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#install docker
+sudo apt-get update
+curl https://get.docker.com | sudo bash
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker $USER
+newgrp docker
+#install docker compose
+sudo apt install -y curl jq
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+#testing webhook
+#login
+echo $DOCKERHUB_PASSWORD | docker login --username sleonard9704 --password-stdin
